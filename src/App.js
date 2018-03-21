fe1be6eaff70799c9ea3b44f9b4b7bf00862d4b5
David Jiménez <davidjj76@gmail.com>
2018-03-21 22:15:14 +0100

    Install axios and fetch github data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
 
+import { getUserData, getRepos } from './services/github-api';
+
 class App extends Component {
+  state = {
+    username: '',
+    repos: null,
+    userData: null,
+  };
+
+  async fetchData() {
+    const { username } = this.state;
+    if (username) {
+      const repos = await getRepos(username);
+      this.setState({ repos }, async () => {
+        const userData = await getUserData(username);
+        this.setState({ userData }, () => {
+          this.setState({ username: '' });
+        });
+      });
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.fetchData();
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const { repos, userData, username } = this.state;
     return (
       <div>
-        Hello world!
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="username"
+            type="text"
+            value={username}
+            onChange={this.handleChange}
+          />
+        </form>
+        <div>REPOS</div>
+        <div>
+          {repos && JSON.stringify(repos)}
+        </div>
+        <div>USER DATA</div>
+        <div>
+          {userData && JSON.stringify(userData)}
+        </div>
       </div>
     );
   }
