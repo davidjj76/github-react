0b3a33b3938b052c9f2637b775da5eaa1cb395d9
David Jiménez <davidjj76@gmail.com>
2018-03-22 01:05:30 +0100

    Refactor withFetchRequest

---
+++ src/modules/withFetchRequest.js
@@ -0,0 +1,27 @@
+import { branch, compose, lifecycle, mapProps, renderComponent } from 'recompose';
+
+import Loader from '../components/loader';
+import Error from '../components/error';
+
+const mapData = ({ data }) => ({ data });
+const mapLoading = () => ({});
+const mapError = ({ error }) => ({ error });
+
+const isLoading = ({ loading }) => loading;
+const renderLoader = compose(mapProps(mapLoading), renderComponent(Loader));
+
+const hasError = ({ error }) => !!error;
+const renderError = compose(mapProps(mapError), renderComponent(Error));
+
+const withFetchRequest = (fetchParams) => compose(
+  lifecycle({
+    componentDidMount() {
+      this.props.fetchRequest(fetchParams);
+    }
+  }),
+  branch(isLoading, renderLoader),
+  branch(hasError, renderError),
+  mapProps(mapData),
+);
+
+export default withFetchRequest;
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -2,9 +2,8 @@ import { connect } from 'react-redux';
 import { branch, compose, lifecycle, renderComponent } from 'recompose';
 
 import User from './user';
-import Loader from '../../components/loader';
-import Error from '../../components/error';
 import * as actions from './actions';
+import withFetchRequest from '../../modules/withFetchRequest';
 
 const mapStateToProps = state => ({
   data: state.user.data,
@@ -16,19 +15,7 @@ const mapDispatchToProps = dispatch => ({
   fetchRequest: username => dispatch(actions.fetchRequest(username)),
 });
 
-const isLoading = ({ loading }) => loading;
-const renderLoader = renderComponent(Loader);
-
-const hasError = ({ error }) => !!error;
-const renderError = renderComponent(Error);
-
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
-  lifecycle({
-    componentDidMount() {
-      this.props.fetchRequest('davidjj76');
-    }
-  }),
-  branch(isLoading, renderLoader),
-  branch(hasError, renderError),
+  withFetchRequest('davidjj76'),
 )(User);
