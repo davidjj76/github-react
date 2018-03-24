bad9658eb5016fabb932a07864a7e15f9765bf28
David Jiménez <davidjj76@gmail.com>
2018-03-25 00:28:41 +0100

    First approach to toggle aside visibility

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,31 +9,76 @@ import User from '../../pages/User';
 import Header from '../Header';
 import Footer from '../Footer';
 
-const App = () => (
-  <React.Fragment>
-    <Header>
-      <nav>
-        <Link to="/">Organizations</Link>
-        <Link to="/repos">Repos</Link>
-      </nav>
-      <Search />
-    </Header>
-    <main style={{ background: 'green', display: 'flex', margin: '3em' }}>
-      <div style={{ flex: 1 }}>
-        <div style={{ background: 'white' }}>
-          <Switch>
-            <Route path="/" exact component={Organizations} />
-            <Route path="/repos" exact component={Repos} />
-            <Route component={() => <div>NOT FOUND!</div>} />
-          </Switch>
+class App extends Component {
+  state = { asideIsOpen: false };
+
+  render() {
+    return (
+      <div
+        style={{
+          background: 'pink',
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          overflowX: 'hidden',
+        }}
+      >
+        <Header>
+          <nav>
+            <Link to="/">Organizations</Link>
+            <Link to="/repos">Repos</Link>
+          </nav>
+          <Search />
+        </Header>
+        <div
+          style={{
+            background: 'yellow',
+            marginTop: '48px',
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <div
+            style={{
+              background: 'blue',
+              flexGrow: 1,
+            }}
+          >
+            <Switch>
+              <Route path="/" exact component={Organizations} />
+              <Route path="/repos" exact component={Repos} />
+              <Route component={() => <div>NOT FOUND!</div>} />
+            </Switch>
+          </div>
+          <div
+            style={{
+              background: 'red',
+              position: 'fixed',
+              top: '48px',
+              left: 0,
+              opacity: this.state.asideIsOpen ? 1 : 0,
+              width: '100%',
+              height: 'calc(100vh - 48px)',
+              transition: 'all 0.5s ease',
+              transform: !this.state.asideIsOpen && 'translateY(48px)',
+            }}
+          >
+            <User />
+          </div>
         </div>
+        <button
+          style={{ position: 'fixed', bottom: '4em', right: '1em' }}
+          onClick={() =>
+            this.setState({ asideIsOpen: !this.state.asideIsOpen })
+          }
+        >
+          {this.state.asideIsOpen ? 'x' : '+'}
+        </button>
+        <Footer>@David Jiménez Jiménez</Footer>
       </div>
-      <div style={{ background: 'blue', maxWidth: 400 }}>
-        <User />
-      </div>
-    </main>
-    <Footer>@David Jiménez Jiménez</Footer>
-  </React.Fragment>
-);
+    );
+  }
+}
 
 export default App;
