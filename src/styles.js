85e908861517ccdd5e04d5a4572c8995aa99aefe
David Jiménez <davidjj76@gmail.com>
2018-03-24 12:33:10 +0100

    Add  roboto font support

--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,8 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
-  @font-face {
-    font-family: 'Roboto';
-    src: url('https://fonts.googleapis.com/css?family=Roboto');
-  }
-
   body {
-    font-family: 'Roboto';
+    font-family: 'Roboto', sans-serif;
     font-size: 100%;
     margin: 0;
   }
