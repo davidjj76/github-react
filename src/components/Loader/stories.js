c4310bc8aec28c134d11387d3228b4c18a1bb4cb
David Jiménez <davidjj76@gmail.com>
2018-03-26 19:28:11 +0200

    Add knobs to storybook

--- src/components/Loader/stories.js
+++ src/components/Loader/stories.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { withKnobs, text, number } from '@storybook/addon-knobs/react';
+
 import Loader from '../Loader';
 
 storiesOf('Loader', module)
+  .addDecorator(withKnobs)
   .add('default', () => <Loader />)
   .add('with text', () => <Loader>Loading user data...</Loader>)
   .add('with size', () => <Loader size={60} />)
-  .add('with colors', () => <Loader circleColor="green" spinColor="red" />);
+  .add('with colors', () => <Loader circleColor="green" spinColor="red" />)
+  .add('with knobs', () => {
+    const children = text('Label', 'Loading user data...');
+    const circleColor = text('Circle color', 'red');
+    const size = number('Size', 30);
+    const spinColor = text('Spin color', 'green');
+    return (
+      <Loader circleColor={circleColor} spinColor={spinColor} size={size}>
+        {children}
+      </Loader>
+    );
+  });
