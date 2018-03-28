22b2d8dbf1da773b36df6fe5d251d19db3132588
David Jiménez <davidjj76@gmail.com>
2018-03-28 07:53:17 +0200

    Add fork icon to repos

--- src/pages/Repos/Repos.js
+++ src/pages/Repos/Repos.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Header, List, Segment } from 'semantic-ui-react';
+import { Header, Icon, Label, List, Segment } from 'semantic-ui-react';
 
 const Repos = ({ data: respositories }) => (
   <Segment basic padded>
     <Header as="h2">Repositories</Header>
     <List divided relaxed size="large">
-      {respositories.map(({ description, id, name, url }) => (
-        <List.Item key={id}>
-          <List.Icon name="github" size="big" verticalAlign="middle" />
-          <List.Content>
-            <List.Header as="a" href={url}>
-              {name}
-            </List.Header>
-            <List.Description>{description}</List.Description>
-          </List.Content>
-        </List.Item>
-      ))}
+      {respositories.map(
+        ({ description, fork, forks_count, id, name, url }) => (
+          <List.Item key={id}>
+            <List.Icon name="github" size="big" verticalAlign="middle" />
+            <List.Content>
+              <List.Header as="a" href={url}>
+                {name}
+              </List.Header>
+              <List.Description>{description}</List.Description>
+              {fork && (
+                <Label>
+                  <Icon name="fork" />
+                  {forks_count}
+                </Label>
+              )}
+            </List.Content>
+          </List.Item>
+        ),
+      )}
     </List>
   </Segment>
 );
